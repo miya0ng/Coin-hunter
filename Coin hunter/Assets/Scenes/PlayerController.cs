973af2d9c5cee361dc4d73c718b4b01d76af5d57
이승연 <leelaf0002@gmail.com>
2025-08-18 15:48:12 +0900

    player 이동 구현

---
+++ Coin hunter/Assets/Scenes/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float speed = 8f;
+
+    private Rigidbody body;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(h, 0f, v);
+        if (velocity.magnitude > 1f)
+        {
+            velocity.Normalize();
+        }
+
+        velocity *= speed;
+        body.linearVelocity = velocity;
+    }
+
+}
