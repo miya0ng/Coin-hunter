a9aa188c364e7845421d612a54bfb0be8607015e
이승연 <leelaf0002@gmail.com>
2025-08-18 17:24:22 +0900

    텍스트 ui 띄우기

--- Coin hunter/Assets/Scenes/GameManager.cs
+++ Coin hunter/Assets/Scenes/GameManager.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem.Interactions;
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject gameOver;
+
+    public TextMeshProUGUI bestRecordText;
+
+    public TextMeshProUGUI timeText;
+    private float coinScore;
+    private bool isGameOver = false;
+
+    private void Start()
     {
-        
+        gameOver.SetActive(false);
+    }
+    private void Update()
+    {
+        if (!isGameOver)
+        {
+            coinScore += Time.deltaTime;
+            timeText.text = $"Score:{Mathf.FloorToInt(coinScore)}";
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndGame()
     {
-        
+        isGameOver = true;
+        gameOver.SetActive(true);
+
+        float bestScore = PlayerPrefs.GetFloat("BestTime", 0f);
+        if (bestScore < coinScore)
+        {
+            bestScore = coinScore;
+            PlayerPrefs.SetFloat("BestTime", bestScore);
+        }
+        bestRecordText.text = $"Best Record:{Mathf.FloorToInt(bestScore)}";
     }
 }
